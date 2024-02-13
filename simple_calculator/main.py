d60fef4c4eb4127ad6ead4b61aa0bf8842817539
kenigteh <artemon1002@gmail.com>
2024-02-13 19:19:05 +0200

    Part 1

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+import operator
+from functools import reduce
+
+
+class SimpleCalculator:
+    def add(self, *args):
+        return sum(args)
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, *args):
+        return reduce(operator.mul, args)
